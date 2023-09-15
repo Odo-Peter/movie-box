f13e0cbcf122966366cd0de90956ad1a934541ec
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-09-15 14:52:50 -0700

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
-function App() {
+import fetchService from './services/fetch';
+import Feed from './pages/Feed';
+import CardDetails from './pages/CardDetails';
+import SearchDetails from './pages/SearchDetails';
+
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const findSearch = async () => {
+  //     const { results } = await fetchService.getSearch(search);
+  //     console.log(results);
+  //     setResults(results);
+  //   };
+  //   findSearch();
+  // }, [search]);
+
+  // useEffect(() => {
+  //   const data = await fetchService.getSearch(search);
+  //     setResults(data.results);
+  // }, [third])
+
+  console.log(results);
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    setSearch('');
+
+    if (!search) {
+      return;
+    }
+    try {
+      const { results } = await fetchService.getSearch(search);
+      // console.log(results);
+      setResults(results);
+      navigate(`/search/${search}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Feed
+              handleSearch={handleSearch}
+              handleSearchChange={handleSearchChange}
+            />
+          }
+        />
+        <Route path="/movies/:id" element={<CardDetails />} />
+        <Route
+          path="/search/:searchTerm"
+          element={<SearchDetails movies={results} />}
+        />
+      </Routes>
+    </main>
   );
-}
+};
 
 export default App;
